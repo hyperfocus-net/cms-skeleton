1d25b772414b7f34fe6017009f1af28f768e147a
verychard <me@verychard.com>
2025-05-19 12:26:57 +0200

    default config fix

--- config/studio.php
+++ config/studio.php
@@ -1,7 +1,10 @@
 <?php
+
 return [
-    'adminLogo'        => '/img/hf_logo.webp',
-    'adminLogoSmall'   => '/img/hf_logo_small.svg',
-    'adminLogoLogin'   => '/img/hf_logo_dark.webp',
-    // 'adminTheme'     => 'dark',
+    'Studio' => [
+        'adminLogo'      => '/img/hf_logo.webp',
+        'adminLogoSmall' => '/img/hf_logo_small.svg',
+        'adminLogoLogin' => '/img/hf_logo_dark.webp',
+        // 'adminTheme'    => 'dark',
+    ],
 ];
